39555eada99e615ff9860306e0955e0b4bf5caaf
realth000 <a598919335@gmail.com>
2023-07-21 04:53:12 +0800

    Update neovim rust-tools plugin config

--- 014-nvim/lua/plugins_config/rust-tools.lua
+++ 014-nvim/lua/plugins_config/rust-tools.lua
@@ -3,12 +3,79 @@ require('rust-tools').setup({
     runnables = {
       use_telescope = true,
     },
+
+    -- inlay_hints = {
+    --   auto = true,
+    --   show_parameter_hints = false,
+    --   parameter_hints_prefix = "",
+    --   other_hints_prefix = "",
+    -- },
+
+    -- These apply to the default RustSetInlayHints command
     inlay_hints = {
+      -- automatically set inlay hints (type hints)
+      -- default: true
       auto = true,
-      show_parameter_hints = false,
-      parameter_hints_prefix = "",
-      other_hints_prefix = "",
+
+      -- Only show inlay hints for the current line
+      only_current_line = true,
+
+      -- whether to show parameter hints with the inlay hints or not
+      -- default: true
+      show_parameter_hints = true,
+
+      -- prefix for parameter hints
+      -- default: "<-"
+      parameter_hints_prefix = "<- ",
+
+      -- prefix for all the other hints (type, chaining)
+      -- default: "=>"
+      other_hints_prefix = "=> ",
+
+      -- whether to align to the length of the longest line in the file
+      max_len_align = false,
+
+      -- padding from the left if max_len_align is true
+      max_len_align_padding = 1,
+
+      -- whether to align to the extreme right or not
+      right_align = false,
+
+      -- padding from the right if right_align is true
+      right_align_padding = 7,
+
+      -- The color of the hints
+      highlight = "Comment",
     },
+
+    -- options same as lsp hover / vim.lsp.util.open_floating_preview()
+    hover_actions = {
+      -- the border that is used for the hover window
+      -- see vim.api.nvim_open_win()
+      border = {
+        { "╭", "FloatBorder" },
+        { "─", "FloatBorder" },
+        { "╮", "FloatBorder" },
+        { "│", "FloatBorder" },
+        { "╯", "FloatBorder" },
+        { "─", "FloatBorder" },
+        { "╰", "FloatBorder" },
+        { "│", "FloatBorder" },
+      },
+
+      -- Maximal width of the hover window. Nil means no max.
+      max_width = nil,
+
+      -- Maximal height of the hover window. Nil means no max.
+      max_height = nil,
+
+      -- whether the hover action window gets automatically focused
+      -- default: false
+      auto_focus = false,
+    },
+
+    -- automatically call RustReloadWorkspace when writing to a Cargo.toml file.
+    reload_workspace_from_cargo_toml = true,
   },
 
   -- all the opts to send to nvim-lspconfig
