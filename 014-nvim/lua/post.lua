7058c6e8225db011042c25336f7773aebddfb0d3
realth000 <a598919335@gmail.com>
2023-08-17 23:41:08 +0800

    Update nvim colorscheme

--- 014-nvim/lua/post.lua
+++ 014-nvim/lua/post.lua
@@ -1,6 +1,11 @@
 
 -- Setup colorscheme
 -- vim.cmd("colorscheme github_dark")
+-- vim.cmd("colorscheme github_dark_dimmed")
+-- vim.cmd("colorscheme github_dark_tritanopia")
+-- vim.cmd("colorscheme github_dark_default")
+-- vim.cmd("colorscheme github_dark_colorblind")
+-- vim.cmd("colorscheme github_dark_high_contrast")
 -- vim.cmd("colorscheme gruvbox")
 -- vim.cmd("colorscheme nightfox")
 -- vim.cmd("colorscheme nordfox")
@@ -13,5 +18,7 @@
 -- vim.cmd("colorscheme tokyonight-storm")
 -- vim.cmd('colorscheme onedark')
 -- vim.cmd('colorscheme catppuccin')
+-- vim.cmd('colorscheme catppuccin-macchiato')
+-- vim.cmd('colorscheme catppuccin-mocha')
 -- vim.cmd('colorscheme hardhacker')
 vim.cmd('colorscheme hardhacker-darker')
