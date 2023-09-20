7386a51c8a9a4d6b75c6a3a6e18c6adaf1541e89
realth000 <a598919335@gmail.com>
2023-09-20 09:41:36 +0800

    Not list tabs in neovim listchar

--- 014-nvim/lua/basic.lua
+++ 014-nvim/lua/basic.lua
@@ -19,8 +19,6 @@ vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 -- vim.opt.list = true
-vim.opt.listchars = 'tab:-->'
--- ↵    ↲
 -- Gruvbox colorscheme need this.
 vim.o.background = 'dark'
 
