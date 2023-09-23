71f5efb2f1c558872f1cbf668cd0d93dbf3e6274
realth000 <a598919335@gmail.com>
2023-09-23 20:22:02 +0800

    Use env to setup neovim theme

--- 014-nvim/lua/post.lua
+++ 014-nvim/lua/post.lua
@@ -1,27 +1,32 @@
--- Setup colorscheme
--- vim.cmd("colorscheme github_dark")
--- vim.cmd("colorscheme github_dark_dimmed")
--- vim.cmd("colorscheme github_dark_tritanopia")
--- vim.cmd("colorscheme github_dark_default")
--- vim.cmd("colorscheme github_dark_colorblind")
--- vim.cmd("colorscheme github_dark_high_contrast")
--- vim.cmd("colorscheme gruvbox")
--- vim.cmd("colorscheme nightfox")
--- vim.cmd("colorscheme nordfox")
--- vim.cmd("colorscheme terafox")
--- vim.cmd("colorscheme carbonfox")
-vim.cmd("colorscheme duskfox")
--- vim.cmd('colorscheme onedark')
--- vim.cmd("colorscheme tokyonight-moon")
--- vim.cmd("colorscheme tokyonight-night")
--- vim.cmd("colorscheme tokyonight-storm")
--- vim.cmd('colorscheme onedark')
--- vim.cmd('colorscheme catppuccin')
--- vim.cmd('colorscheme catppuccin-macchiato')
--- vim.cmd('colorscheme catppuccin-mocha')
--- vim.cmd('colorscheme hardhacker')
--- vim.cmd('colorscheme hardhacker-darker')
-
 -- Set floating window background color to transparent after loading colorscheme
 vim.api.nvim_set_hl(0, 'NormalFloat', {bg='None'})
+
+-- Setup colorscheme
+-- github_dark
+-- github_dark_dimmed
+-- github_dark_tritanopia
+-- github_dark_default
+-- github_dark_colorblind
+-- github_dark_high_contrast
+-- gruvbox
+-- nightfox
+-- nordfox
+-- terafox
+-- carbonfox
+-- duskfox
+-- onedark
+-- tokyonight-moon
+-- tokyonight-night
+-- tokyonight-storm
+-- onedark
+-- catppuccin
+-- catppuccin-macchiato
+-- catppuccin-mocha
+-- hardhacker
+-- hardhacker-darker
+
+local colorscheme = os.getenv('NVIM_CUSTOM_COLORSCHEME')
+if (colorscheme) then
+	vim.cmd('colorscheme ' .. colorscheme)
+end
 
