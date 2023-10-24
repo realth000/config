410ce2ebdc8039315f4059a575b23469f51f9186
realth000 <a598919335@gmail.com>
2023-10-24 10:03:41 +0800

    Disable neovim noice.nvim notify

--- 014-nvim/after/plugin/noice.lua
+++ 014-nvim/after/plugin/noice.lua
@@ -97,7 +97,7 @@ plugin.setup({
 		-- event is always 'notify' and kind can be any log level as a string
 		-- The default routes will forward notifications to nvim-notify
 		-- Benefit of using Noice for this is the routing and consistent history view
-		enabled = true,
+		enabled = false,
 		view = 'notify',
 	},
 	lsp = {
