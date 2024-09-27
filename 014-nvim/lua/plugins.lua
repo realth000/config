aba242d47bacc1fc4ca12856053ea6d3707f7f57
realth000 <a598919335@gmail.com>
2024-09-27 08:57:55 +0800

    remove markdown preview plugin

--- 014-nvim/lua/plugins.lua
+++ 014-nvim/lua/plugins.lua
@@ -138,16 +138,4 @@ return require('lazy').setup({
 	{
 		'LhKipp/nvim-nu',
 	},
-
-	-- Preview markdown
-	{
-		"iamcco/markdown-preview.nvim",
-		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-		build = "cd app && npm install",
-		init = function()
-			vim.g.mkdp_filetypes = { "markdown" }
-		end,
-		ft = { "markdown" },
-	},
-
 })
