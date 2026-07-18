ce95958fcbe6d90910bf1d88d280d7a4c8f9f34f
realth000 <a598919335@gmail.com>
2026-07-18 19:23:53 +0800

    feat(nvim): update blink.cmp completion menu style

--- 014-nvim/after/plugin/blink.cmp.lua
+++ 014-nvim/after/plugin/blink.cmp.lua
@@ -18,14 +18,39 @@ plugin.setup({
 
 	-- (Default) Only show the documentation popup when manually triggered
 	completion = {
-		documentation = { auto_show = false },
+		keyword = { range = 'prefix' },
+		documentation = {
+			auto_show = true,
+			auto_show_delay_ms = 400,
+		},
 		ghost_text = { enabled = true },
 		list = {
 			selection = {
 				preselect = true,
 				auto_insert = false,
 			},
-		}
+		},
+		menu = {
+			draw = {
+				components = {
+					label = {
+						-- Compared to the original label component, removed "label_detail":
+						--
+						-- ```lua
+						-- text = function(ctx) return ctx.label .. ctx.label_detail end,
+						-- ```
+						text = function(ctx) return ctx.label end,
+					},
+					-- An empty component works as a placeholder.
+					space = { text = function (ctx) return ' ' end, },
+				},
+				columns = {
+					-- Disable "label_description", it is useless.
+					{ "kind_icon", "label", gap = 1 },
+					{ "kind" },
+				},
+			},
+		},
 	},
 
 	-- (Default) list of enabled providers defined so that you can extend it
